d78b577ec285ff34f2da0258a44f1435a4b317a0
Bartosz Rataj <255684@edu.p.lodz.pl>
2025-01-30 09:03:23 +0100

    01 zmiana metody policz

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
+int mnozenie(int a, int b){return a*b;}
+
 int main(void) {
     printf("Hello, World!\n");
+    printf("Pierwszy branch: %d", mnozenie(2,3));
     return 0;
 }
